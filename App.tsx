49c5abe6ad1b473ae0c437e9f1e98b947152e881
riansco14 <rian.rabelo@ufrpe.br>
2021-06-25 04:13:20 -0300

    Instalação do Background com LinearGradient

--- App.tsx
+++ App.tsx
@@ -4,28 +4,29 @@ import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font'
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani'
-
 import { StatusBar, StyleSheet } from 'react-native'
+
+import { Background } from './src/components/Background';
 import { SignIn } from './src/screens/SignIn'
 
 export default function App() {
   let [fontsLoaded] = useFonts({
-    Inter_500Medium, Inter_400Regular,
+    Inter_400Regular, Inter_500Medium,
     Rajdhani_500Medium, Rajdhani_700Bold
   })
 
   if (!fontsLoaded) {
-    <AppLoading />
+    return <AppLoading />
   }
 
   return (
-    <>
+    <Background>
       <StatusBar barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <SignIn />
-    </>
+    </Background>
   )
 }
 

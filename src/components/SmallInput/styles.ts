673030a11efa2439c348b87dea54477d1c45df2b
riansco14 <rian.rabelo@ufrpe.br>
2021-07-07 21:58:28 -0300

    Componentes de texto

---
+++ src/components/SmallInput/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/theme";
+
+export const styles = StyleSheet.create({
+    container: {
+        width: 48,
+        height: 48,
+        backgroundColor: theme.colors.secondary40,
+        color: theme.colors.heading,
+        borderRadius: 8,
+        fontSize: 13
+    }
+})
---
+++ src/components/TextArea/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/theme";
+
+export const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 95,
+        backgroundColor: theme.colors.secondary40,
+        color: theme.colors.heading,
+        borderRadius: 8,
+        fontSize: 13,
+        textAlign: 'center'
+    }
+})
